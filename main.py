c454701253ca674a5457a7c41457c88b11ad65f3
Kevin Burrows <Kevin.Burrows@ge.com>
2019-11-18 23:27:59 -0500

    Begin adding the ability to get team info

--- main.py
+++ main.py
@@ -2,41 +2,22 @@ import datetime
 import json
 import time
 
-from player import Player
+from yahoo.fantasy_hockey_api import FantasyHockeyApi
 from team import Team
-from yahoo import Yahoo
 
 GAME = 386
 LEAGUE = 75370
 TEAM = 5
 
-base_url = "https://fantasysports.yahooapis.com/fantasy/v2/"
-game_url = "game/" + str(GAME) + "/"
-league_url = "league/" + str(GAME) + ".l." + str(LEAGUE) + "/"
-team_url = "team/" + str(GAME) + ".l." + str(LEAGUE) + ".t." + str(TEAM) + "/"
-roster_url = team_url + "roster/"
-player_url = league_url + "players"
-
-def get_total_starts():
-    players = []
-    yahoo = Yahoo('oauth.json')
-
-    # Get players
-    raw_player_data = yahoo.get(base_url + roster_url)['fantasy_content']['team'][1]['roster']['0']['players']
-    for p in raw_player_data:
-        if p != 'count':
-            players.append(Player(raw_player_data[p]['player']))
-
-    # Get teams and schedules
-    teams = []
-    team_keys = set([ x.team_key for x in players])
-    for tk in team_keys:
-        raw_team_data = yahoo.get(base_url + 'team/' + str(GAME) + '.l.' + str(LEAGUE) + '.t.' + str(tk))['fantasy_content']
-        None
-
+def load_all_team_info(api):
+    teams = [Team(x) for x in api.get_all_teams()]
 
 def main():
-    get_total_starts()
+    api = FantasyHockeyApi('oauth.json', LEAGUE)
+    test = api.get_team_matchups(1)
+    #load_all_team_info(api)
+    # get_total_starts(api)
+    None
 
 if __name__ == "__main__":
     main()
--- team.py
+++ team.py
@@ -1,5 +1,8 @@
-
 class Team:
-    def __init__(self, players, schedule):
-        None
-        
+    def __init__(self, raw_team_info, roster_info = None, matchup_info = None):
+        self.name = raw_team_info['name']
+        self.id = raw_team_info['team_id']
+        self.is_my_team = bool(raw_team_info['is_owned_by_current_login'])
+        self.waiver_priority = raw_team_info['waiver_priority']
+        self.move_count = raw_team_info['number_of_moves']
+        self.trade_count = raw_team_info['number_of_trades']
